93d0e0c48d13f809c8f5cfd3127c2e5c21b63923
Kelly L. Rowland <kellyrowland@lbl.gov>
2019-08-21 18:24:09 +0000

    adding mean files

---
+++ mean.py
@@ -0,0 +1,8 @@
+def mean(num_list):
+    try:
+        return sum(num_list)/len(num_list)
+    except ZeroDivisionError :
+        return 0
+    except TypeError as detail:
+        msg = "Please provide a list of numbers."
+        raise TypeError(detail.__str__() + "\n" + msg)
